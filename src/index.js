aa8faa296cb55b415f7f1b707fd0f99c5b7a448b
jaldalla <jasimm7md@gmail.com>
2025-03-15 10:50:04 +0300

    added readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <Router basename="/graphql">
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
